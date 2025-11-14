60b74431d4d020efae0f931fdbe615881714174e
wonehgus <wonehgus1234@naver.com>
2025-11-14 14:10:48 +0900

    랭킹 페이지 디자인 개선

--- src/main/java/project7/website/Database/Repository/RankingItemDTO.java
+++ src/main/java/project7/website/Database/Repository/RankingItemDTO.java
@@ -1,4 +1,4 @@
 package project7.website.Database.Repository;
 
-public record RankingItemDTO(String img, String name, String brand, String price) {
+public record RankingItemDTO(String img, String url,String name, String brand, String price) {
 }
