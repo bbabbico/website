db048e1dc7d3b6bdb8ded4686001b875d70eedae
wonehgus <wonehgus1234@naver.com>
2025-11-22 20:52:46 +0900

    사이트별 랭킹 불러오기 구현 완료

--- src/main/java/project7/website/Controller/index/rankingController.java
+++ src/main/java/project7/website/Controller/index/rankingController.java
@@ -8,6 +8,9 @@ import project7.website.Database.Repository.RankingItemRepository;
 import project7.website.Database.member.Member;
 import project7.website.session.SessionConst;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class rankingController {
     @GetMapping("/ranking")
@@ -26,7 +29,7 @@ public class rankingController {
     //랭킹 불러오기 API
     @ResponseBody
     @PostMapping("/ranking")
-    public RankingItemRepository LoadRanking() {
+    public Map<String, RankingItemRepository> LoadRanking() {
         RankingItemRepository repo = new RankingItemRepository();
         repo.addRankingItemDTOList(new RankingItemDTO("https://image.oliveyoung.co.kr/cfimages/cf-goods/uploads/images/thumbnails/400/10/0000/0023/A00000023633808ko.jpg?l=ko",
                 "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000236338&dispCatNo=90000010009&trackingCd=Best_Sellingbest&t_page=%EB%9E%AD%ED%82%B9&t_click=%ED%8C%90%EB%A7%A4%EB%9E%AD%ED%82%B9_%EC%A0%84%EC%B2%B4_%EC%83%81%ED%92%88%EC%83%81%EC%84%B8&t_number=1",
@@ -58,7 +61,14 @@ public class rankingController {
                 "[11월 올영픽] 에스트라 아토베리어365 크림 80ml 더블 기획(+에센스 25ML + 세라-히알 앰플 7ML)",
                 "300,000")
         );
-        return repo;
+        Map<String, RankingItemRepository> result = new HashMap<>();
+
+        result.put("oliveyoung", repo);
+        result.put("qoo10", repo);
+        result.put("coupang", repo);
+        result.put("amazon", repo);
+
+        return result;
     }
 
     //실험용페이지
