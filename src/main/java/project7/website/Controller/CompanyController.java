199de00b2ee9d71453af3d9a1c237f006aafd83c
wonehgus <wonehgus1234@naver.com>
2025-12-07 11:52:21 +0900

    기업 비교 페이지 신설

---
+++ src/main/java/project7/website/Controller/CompanyController.java
@@ -0,0 +1,25 @@
+package project7.website.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import project7.website.Database.member.Member;
+import project7.website.session.SessionConst;
+
+@Controller
+public class CompanyController {
+
+    @GetMapping("/company")
+    public String company(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember , Model model) {
+        //세션에 회원 데이터가 없으면 빈 Model 전달
+        if (loginMember == null) {
+            return  "mainmenu/company";
+        }
+
+        //로그인 세션 존재하면 view 전달
+        model.addAttribute("member", loginMember);
+        return  "mainmenu/company";
+
+    }
+}
